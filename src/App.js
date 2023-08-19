eb473ebbbecdd3f36fa6551370b0eab355d831d7
Larissa Couto Ferreira <laaricouto@gmail.com>
2023-08-19 00:07:38 -0300

    criado projeto-atual

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import { Container, Typography } from '@mui/material';
+import '@fontsource/roboto/';
+class App extends Component {
+  
+  render(){
+    return (
+      <Container component="article" maxWidth="sm">
+       <Typography variant="h3" component="h1" align="center">Formulário de cadastro</Typography>
+      <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} /> 
+      </Container>
+    
+    );
+  }
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function aoEnviarForm(dados){
+console.log(dados)
+}
+
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"CPF deve ter 11 dígitos"}
+  }else{
+    return {valido:true, texto:""}
+  }
 }
 
 export default App;
